bc0e78975290dcfd33cc5710f4c0cb574f1699e2
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2026-09-08 10:50:15 +0300

    netlify 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+// import type { NextConfig } from 'next';
+
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // Turn off disk caching for dev, build, or both
+    turbopackFileSystemCacheForDev: false,
+    turbopackFileSystemCacheForBuild: false,
+  },
 };
 
 export default nextConfig;
